c7fba971f1826ce71defcd4ca5cfc9343d1778a8
yilmaz1998 <snn.ylmz9804@gmail.com>
2026-03-13 15:37:37 -0400

    feat: add route structures

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
-const App = () => {
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Chat from "./pages/Chat";
+
+const Layout = () => {
   return (
-    <div className="text-2xl text-center">Hello</div>
-  )
-}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+)};
+
+const App = () => (
+  <BrowserRouter>
+    <Layout />
+  </BrowserRouter>
+);
 
 export default App
