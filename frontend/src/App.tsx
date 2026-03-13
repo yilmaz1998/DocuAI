f4afdab3ee96e7925998475305c2a2ddf3276050
yilmaz1998 <snn.ylmz9804@gmail.com>
2026-03-13 18:04:28 -0400

    feat: add smooth animations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,45 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Chat from "./pages/Chat";
+import { AnimatePresence, motion } from "framer-motion";
+
 
 const Layout = () => {
+  const location = useLocation();
+
   return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chat" element={<Chat />} />
-      </Routes>
-)};
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+      <Route
+        path="/"
+        element={
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Home />
+          </motion.div>
+        }
+      />
+      <Route
+        path="/chat"
+        element={
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Chat />
+          </motion.div>
+        }
+      />
+    </Routes>
+    </AnimatePresence >
+  );
+};
 
 const App = () => (
   <BrowserRouter>
--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { axiosInstance } from '../lib/axios'
+import { motion, AnimatePresence } from 'framer-motion'
+import { Link } from 'react-router-dom';
 
 interface Source {
   topic: string;
@@ -43,40 +45,56 @@ const Chat = () => {
         placeholder="Try: What is a React hook?"
         value={question}
         onChange={(e) => setQuestion(e.target.value)}
-        className="w-full p-3 border rounded-lg mb-3 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
       />
 
-      <button
-        className="w-1/3 bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition-colors mb-6"
-        onClick={handleAsk}
-        disabled={loading}
-      >
-        {loading ? 'Asking...' : 'Ask DocuAI'}
-      </button>
+      <div className="flex flex-col sm:flex-row gap-2 justify-center mb-6 mt-6">
+        <Link to="/" className="flex-1">
+          <button className="w-full bg-red-500 text-white p-3 rounded-lg hover:bg-red-600 transition-colors">
+            Go Back
+          </button>
+        </Link>
 
-      {answer && (
-        <div className="mt-6 p-4 bg-gray-50 rounded-lg shadow-lg shadow-blue-200">
-          <p className='text-xl font-semibold text-blue-500 mb-1'>DocuAI's Answer:</p>
-          <p className='bg-blue-50 px-3 py-2 rounded hover:bg-blue-100 transition-colors'>{answer}</p>
+        <button
+          className="flex-1 w-full bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition-colors"
+          onClick={handleAsk}
+          disabled={loading}
+        >
+          {loading ? 'Asking...' : 'Ask DocuAI'}
+        </button>
+      </div>
 
-          {sources.length > 0 && (
-            <div className="mt-6 rounded-lg p-2">
-              <p className="text-xl font-semibold text-blue-500 mb-1">Sources:</p>
-              <ul>
-                {sources.map((source, index) => (
-                  <li
-                    key={index}
-                    className="items-center justify-between bg-blue-50 py-2 rounded hover:bg-blue-100 transition-colors"
-                  >
-                    <span className="font-medium text-gray-800">{source.topic} - </span>
-                    <span className="text-gray-500 text-sm">{source.source}</span>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-        </div>
-      )}
+      <AnimatePresence>
+        {answer && (
+          <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.5 }}
+            className="mt-6 p-4 bg-gray-50 rounded-lg shadow-lg shadow-blue-200"
+          >
+            <p className='text-xl font-semibold text-blue-500 mb-1'>DocuAI's Answer:</p>
+            <p className='bg-blue-50 px-3 py-2 rounded hover:bg-blue-100 transition-colors'>{answer}</p>
+
+            {sources.length > 0 && (
+              <div className="mt-6 rounded-lg p-2">
+                <p className="text-xl font-semibold text-blue-500 mb-1">Sources:</p>
+                <ul>
+                  {sources.map((source, index) => (
+                    <li
+                      key={index}
+                      className="items-center justify-between bg-blue-50 py-2 rounded hover:bg-blue-100 transition-colors"
+                    >
+                      <span className="font-medium text-gray-800">{source.topic} - </span>
+                      <span className="text-gray-500 text-sm">{source.source}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   )
 }
